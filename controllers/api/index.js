86b6797589938edaaf383e2097142670bf4dca0c
Giselle <martinezgiselle5@gmail.com>
2023-09-12 21:26:17 -0400

    Troubleshooting route for login

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const ChildrenRoutes = require('./ChildrenRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/children', ChildrenRoutes);
 
 module.exports = router;
