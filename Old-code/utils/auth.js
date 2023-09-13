e294b52cec6d79a35fe83321b15bd8078b4eb24b
wrenjohnson <wrenjohnson22@gmail.com>
2023-09-12 20:30:43 -0400

    started css and started creating new signup route

--- Old-code/utils/auth.js
+++ Old-code/utils/auth.js
@@ -7,4 +7,14 @@ const withAuth = (req, res, next) => {
   }
 };
 
+const withAuth2 = (req, res, next) => {
+  // If the user is not logged in, redirect the request to the login route
+  if (!req.session.logged_in) {
+    res.redirect('/signup');
+  } else {
+    next();
+  }
+};
+
 module.exports = withAuth;
+module.exports = withAuth2;
