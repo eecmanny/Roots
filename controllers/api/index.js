d289fc82784a2e30e43d5017d4a1d83692b92a94
eecmanny <eec123manny@gmail.com>
2023-09-16 13:38:23 -0400

    gchild-routes created

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const ChildrenRoutes = require('./ChildrenRoutes');
+const GrandChildrenRoutes = require('./ChildrenRoutes');
 
 router.use('/users', userRoutes);
+
 //http://localhost:3001/api/children/
 router.use('/children', ChildrenRoutes);
+
+//http://localhost:3001/api/children/
+router.use('/grandchildren', GrandChildrenRoutes);
 
 module.exports = router;
